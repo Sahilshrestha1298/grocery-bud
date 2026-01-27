47f3a7559a44305416ab85f08229ad46a1b9bf41
Sahilshrestha1298 <shresthasahil24@gmail.com>
2026-01-27 09:55:39 +0545

    updated app

--- js/app.js
+++ js/app.js
@@ -12,3 +12,13 @@ function render() {
 }
 
 render();
+
+export function editCompleted(itemId) {
+  items = items.map((item) => {
+    if (item.id === itemId) {
+      return { ...item, completed: !item.completed };
+    }
+    return item;
+  });
+  render();
+}
